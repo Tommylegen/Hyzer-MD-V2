d54abd50dbaa05b239c0726f9d3dc03d98c2126d
Tommylegen <102867229+Tommylegen@users.noreply.github.com>
2022-04-13 20:49:56 +0700

    Update caklontong.js

--- plugins/caklontong.js
+++ plugins/caklontong.js
@@ -1,8 +1,6 @@
 let fetch = require('node-fetch')
-
 let timeout = 120000
 let poin = 500
-let src
 let handler = async (m, { conn, usedPrefix }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
     let id = m.chat
@@ -10,7 +8,7 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.caklontong[id][0])
         throw false
     }
-    if (!src) src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/caklontong.json')).json()
+    let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/caklontong.json')).json()
     let json = src[Math.floor(Math.random() * src.length)]
     let caption = `
 ${json.soal}
@@ -20,10 +18,10 @@ Ketik ${usedPrefix}calo untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.caklontong[id] = [
-        await conn.sendBut(m.chat, caption, author, 'Bantuan', '.calo', m),
+        await conn.sendButton(m.chat, caption, watermark, 'Bantuan', '.calo', m),
         json, poin,
         setTimeout(async () => {
-            if (conn.caklontong[id]) conn.sendBut(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.deskripsi}`, wm, 'Cak Lontong', '.caklontong', conn.caklontong[id][0])
+            if (conn.caklontong[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.deskripsi}`, watermark, 'Cak Lontong', '.caklontong', m)
             delete conn.caklontong[id]
         }, timeout)
     ]
